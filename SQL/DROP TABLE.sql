b110bcd4933fd10f18a0fd801f240aa2fad98bed
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-08-15 18:16:05 -0700

    dgw

---
+++ SQL/DROP TABLE.sql
@@ -0,0 +1,3 @@
+-- !preview conn=conn
+
+DROP TABLE [dbo].[Cyclistic_divvy_tripdata];
--- SQL/InsertTable_dibby_tripdata.sql
+++ SQL/InsertTable_dibby_tripdata.sql
@@ -2,33 +2,33 @@
 -- !preview conn=conn
 
 /*
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202201-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202202-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202203-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202204-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202205-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202206-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202207-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202208-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202209-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202210-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202211-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202212-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202301-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202302-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202303-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202304-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202305-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202306-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202307-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202308-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202201-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202202-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202203-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202204-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202205-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202206-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202207-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202208-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202209-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202210-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202211-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202212-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202301-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202302-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202303-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202304-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202305-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202306-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202307-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202308-divvy-tripdata.csv'
 */
 
 
 
 
 BULK INSERT Cyclistic_divvy_tripdata
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202201-divvy-tripdata.csv'
+FROM 'C:\Users\darre\OneDrive\Documents\!Datasets\Cyclistic_divvy_tripdata CSVs\CSVs\202201-divvy-tripdata.csv'
 
 WITH (
    FIELDTERMINATOR = ',',
@@ -36,44 +36,3 @@ WITH (
    FIRSTROW = 2
 );
 
-
-=======
--- !preview conn=conn
-
-/*
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202201-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202202-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202203-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202204-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202205-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202206-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202207-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202208-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202209-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202210-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202211-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202212-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202301-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202302-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202303-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202304-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202305-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202306-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202307-divvy-tripdata.csv'
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202308-divvy-tripdata.csv'
-*/
-
-
-
-
-BULK INSERT Cyclistic_divvy_tripdata
-FROM 'C:\Users\darre\OneDrive\Documents\Coursera_Capstone_Project\CSV\202201-divvy-tripdata.csv'
-
-WITH (
-   FIELDTERMINATOR = ',',
-   ROWTERMINATOR = '\n',
-   FIRSTROW = 2
-);
-
-
->>>>>>> 24b99bb47f90bc8d6f4d6572d35b2d444c47e7b1
