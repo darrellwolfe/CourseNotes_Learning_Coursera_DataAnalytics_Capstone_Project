57ebcefbe5d8f673d27c77bd25df4a8e90b945cd
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-08-18 23:52:11 -0700

    Data Ready

--- SQL/CheckResults.sql
+++ SQL/CheckResults.sql
@@ -2,3 +2,75 @@
 
 SELECT TOP 10 *
 FROM dbo.Cyclistic_divvy_tripdata
+ORDER BY started_at DESC
+
+
+SELECT
+rideable_type,
+COUNT(ride_id) AS Trips
+FROM dbo.Cyclistic_divvy_tripdata
+GROUP BY rideable_type;
+
+
+-- Checking for duplicate ride_id since this was part of the reason my bulk inserts weren't working according to the error messages
+WITH CTE AS (
+  SELECT 
+    ROW_NUMBER() OVER (PARTITION BY ride_id ORDER BY ride_id) AS rn,
+    *
+  FROM [dbo].[Cyclistic_divvy_tripdata]
+)
+
+SELECT *
+FROM CTE
+WHERE CTE.rn > 1
+
+-- After running DELETEs, no Row Numbers greater than 1, therefore all duplicates removed. 
+
+
+SELECT 
+member_casual,
+AVG(ride_length) AS Average_Ride_Length
+FROM dbo.Cyclistic_divvy_tripdata
+GROUP BY member_casual
+
+
+--MEDIAN(ride_length) AS Median_Ride_Length -- No MEDIAN in SQL Server Lite
+-- Optional way to accomplish this (probably just do it in R)
+-- This approach resulted in NULL Median
+SELECT 
+    member_casual,
+    AVG(ride_length) AS Average_Ride_Length,
+    (
+        SELECT AVG(1.0 * sub.ride_length) AS median
+        FROM (
+            SELECT ride_length, ROW_NUMBER() OVER (ORDER BY ride_length) AS rn1,
+                   ROW_NUMBER() OVER (ORDER BY ride_length DESC) AS rn2
+            FROM dbo.Cyclistic_divvy_tripdata
+            WHERE member_casual = main.member_casual
+        ) AS sub
+        WHERE rn1 IN (rn2, rn2 - 1, rn2 + 1)
+    ) AS Median_Ride_Length
+FROM dbo.Cyclistic_divvy_tripdata AS main
+GROUP BY member_casual;
+
+
+-- MODE is Excel, in SQL we can:
+SELECT 
+member_casual,
+day_of_week,
+ride_length
+
+FROM (
+  SELECT member_casual,
+        day_of_week,
+        ride_length,
+         ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS Rank
+  FROM [dbo].[Cyclistic_divvy_tripdata]
+  GROUP BY 
+        member_casual,
+        day_of_week,
+        ride_length
+
+) AS Subquery
+WHERE Rank = 1;
+
--- SQL/CreateTable_Cyclistic_divvy_tripdata.sql
+++ SQL/CreateTable_Cyclistic_divvy_tripdata.sql
@@ -1,7 +1,8 @@
 -- !preview conn=conn
+--  PRIMARY KEY became an issue while importing data, removing Primary Key component for now.
 
 CREATE TABLE [dbo].[Cyclistic_divvy_tripdata] (
-    ride_id VARCHAR(60) PRIMARY KEY,
+    ride_id VARCHAR(60), 
     rideable_type VARCHAR(MAX),
     started_at DATETIME,
     ended_at DATETIME,
@@ -15,3 +16,56 @@ CREATE TABLE [dbo].[Cyclistic_divvy_tripdata] (
     end_lng DECIMAL(22, 20),    
     member_casual VARCHAR(MAX)
 );
+
+
+
+ALTER TABLE [dbo].[Cyclistic_divvy_tripdata]
+ADD month_started_at INT,
+    year_started_at INT;
+
+
+UPDATE [dbo].[Cyclistic_divvy_tripdata]
+SET month_started_at = MONTH(started_at),
+    year_started_at = YEAR(started_at);
+
+
+
+ALTER TABLE [dbo].[Cyclistic_divvy_tripdata]
+ADD ride_length INT,
+    day_of_week VARCHAR(9);
+
+
+ALTER TABLE [dbo].[Cyclistic_divvy_tripdata]
+ADD month_name VARCHAR(20);
+
+-- GPT-4 tried to follow this with from parts, but we have the original date, so I changed this to be simpler.
+-- UPDATE [dbo].[Cyclistic_divvy_tripdata]
+-- SET month_name = DATENAME(MONTH, DATEFROMPARTS(year_started_at, month_started_at, 1));
+
+UPDATE [dbo].[Cyclistic_divvy_tripdata]
+SET month_name = DATENAME(MONTH, started_at);
+
+
+DELETE FROM [dbo].[Cyclistic_divvy_tripdata]
+WHERE ride_id IS NULL
+  AND rideable_type IS NULL
+  AND started_at IS NULL
+  AND ended_at IS NULL
+  AND start_station_name IS NULL
+  AND member_casual IS NULL;
+
+
+WITH CTE AS (
+  SELECT 
+    ROW_NUMBER() OVER (PARTITION BY ride_id ORDER BY ride_id) AS rn,
+    *
+  FROM [dbo].[Cyclistic_divvy_tripdata]
+)
+DELETE FROM CTE WHERE rn > 1;
+
+
+
+
+
+
+
